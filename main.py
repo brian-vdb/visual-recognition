e27c12d1f4dfcd8c126914afb1db99f066eeb057
bergb15 <brianvdberg2107@gmail.com>
2024-01-10 11:55:16 +0100

    Loaded main input args

--- main.py
+++ main.py
@@ -3,24 +3,36 @@ import os
 import argparse
 import sys
 
+
+# Standard Constants
+STD_MODELS_FOLDER = "models"
+STD_TEST_FOLDER = "test"
+
 def main():
     """"""
 
 if __name__ == "__main__":
     # Create argument parser
-    parser = argparse.ArgumentParser(description="Process faceboxes and save eigenface images.")
+    parser = argparse.ArgumentParser(description="Apply the trained models to a folder of test data.")
     
     # Add arguments
     parser.add_argument("--models", type=str, help="Name of the models folder")
+    parser.add_argument("--test", type=str, help="Name of the folder with test images")
 
     # Parse arguments
     args = parser.parse_args()
 
-    # Check if the annotations file exists
-    annotations = args.annotations
-    if not os.path.isfile(annotations):
-        print(f"Error: Annotations file '{annotations}' does not exist. Please provide a valid file.")
-        sys.exit(1)
+    # Check the models input
+    models_path = args.models
+    if models_path == None:
+        models_path = STD_MODELS_FOLDER
+
+    # Check the test input
+    test_path = args.test
+    if test_path == None:
+        test_path = STD_TEST_FOLDER
+
+    print(f'{models_path}, {test_path}')
     
     # Call the main
-    main(annotations)
+    main()
