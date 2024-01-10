52c6f675decc7887d796e4c8457d6a93486f2205
bergb15 <brianvdberg2107@gmail.com>
2024-01-10 12:11:37 +0100

    Completed main input

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import sys
 # Standard Constants
 STD_MODELS_FOLDER = "models"
 STD_TEST_FOLDER = "test"
+IMAGE_EXTENSIONS = ['.jpg', '.jpeg', '.png']
 
 def main():
     """"""
@@ -17,22 +18,35 @@ if __name__ == "__main__":
     
     # Add arguments
     parser.add_argument("--models", type=str, help="Name of the models folder")
-    parser.add_argument("--test", type=str, help="Name of the folder with test images")
+    parser.add_argument("--test", type=str, help="Folder to get the test images from")
 
     # Parse arguments
     args = parser.parse_args()
 
-    # Check the models input
-    models_path = args.models
-    if models_path == None:
-        models_path = STD_MODELS_FOLDER
+    # Manage models folder input
+    models_folder = args.models
+    if models_folder == None and os.path.exists(STD_MODELS_FOLDER):
+        models_folder = STD_MODELS_FOLDER
+    elif not os.path.exists(STD_MODELS_FOLDER):
+        print(f'Error: Models folder not provided and {STD_MODELS_FOLDER} does not exist. Please provide a valid folder.')
+        sys.exit(1)
 
-    # Check the test input
-    test_path = args.test
-    if test_path == None:
-        test_path = STD_TEST_FOLDER
-
-    print(f'{models_path}, {test_path}')
+    # Check if the expected model files exist
+    expected_model_files = ['best_eigenfaces.npy', 'cascade.xml', 'mean_face.npy' ,'recognizer.yml']
+    missing_model_files = []
+    for filename in expected_model_files:
+        if not os.path.isfile(os.path.join(models_folder, filename)):
+            missing_model_files.append(filename)
+    if len(missing_model_files) > 0:
+        print(f'Error: The following expected model files were missing: {missing_model_files}')
+    
+    # Manage test folder input
+    test_folder = args.test
+    if test_folder == None and os.path.exists(STD_TEST_FOLDER):
+        test_folder = STD_TEST_FOLDER
+    elif not os.path.exists(STD_TEST_FOLDER):
+        print(f'Error: Test folder not provided and {STD_TEST_FOLDER} does not exist. Please provide a valid folder.')
+        sys.exit(1)
     
     # Call the main
     main()
